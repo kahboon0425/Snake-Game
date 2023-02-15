ba9c58d72031976634ff53cdd2f83910390067d8
kahboon0425 <changkahboon25@gmail.com>
2023-02-15 20:07:54 +0800

    2ndCommit

--- main.py
+++ main.py
@@ -37,15 +37,15 @@ while game_is_on:
 
     # TODO 6: Detect collision with wall
     if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.highest_score()
+        snake.reset()
 
     # TODO 7: Detect collision with tail
     # if head collides with any segment in the tail, trigger game over
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.highest_score()
+            snake.reset()
 
 
 
